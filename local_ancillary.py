f51bda1954d2b5bb3959a8690d10e0c27c6ebe2c
hvgazula <hvgazula@umich.edu>
2018-06-15 20:55:05 -0600

    updated local_ancillary to be consistent with coinstac_ssr_fsl

--- local_ancillary.py
+++ local_ancillary.py
@@ -61,7 +61,7 @@ def gather_local_stats(X, y):
     return (params, sse, tvalues, rsquared, dof_global)
 
 
-def local_stats_to_dict_numba(X, y):
+def local_stats_to_dict_vbm(X, y):
     """Wrap local statistics into a dictionary to be sent to the remote"""
     X1 = sm.add_constant(X).values.astype('float64')
     y1 = y.values.astype('float64')
@@ -70,12 +70,15 @@ def local_stats_to_dict_numba(X, y):
 
     pvalues = 2 * sp.stats.t.sf(np.abs(tvalues), dof_global)
 
-    keys = ["beta", "sse", "pval", "tval", "rsquared"]
+    keys = [
+        "Coefficient", "Sum Square of Errors", "t Stat", "P-value", "R Squared"
+    ]
 
     values1 = pd.DataFrame(
         list(
-            zip(params.T.tolist(), sse.tolist(), pvalues.T.tolist(),
-                tvalues.T.tolist(), rsquared.tolist())),
+            zip(params.T.tolist(),
+                sse.tolist(),
+                tvalues.T.tolist(), pvalues.T.tolist(), rsquared.tolist())),
         columns=keys)
 
     local_stats_list = values1.to_dict(orient='records')
@@ -85,44 +88,77 @@ def local_stats_to_dict_numba(X, y):
     return beta_vector, local_stats_list
 
 
-def local_stats_to_dict(X, y):
+def ignore_nans(X, y):
+    """Removing rows containing NaN's in X and y"""
+
+    if type(X) is pd.DataFrame:
+        X_ = X.values.astype('float64')
+    else:
+        X_ = X
+
+    if type(y) is pd.Series:
+        y_ = y.values.astype('float64')
+    else:
+        y_ = y
+
+    finite_x_idx = np.isfinite(X_).all(axis=1)
+    finite_y_idx = np.isfinite(y_)
+
+    finite_idx = finite_y_idx & finite_x_idx
+
+    y_ = y_[finite_idx]
+    X_ = X_[finite_idx, :]
+
+    return X_, y_
+
+
+def local_stats_to_dict_fsl(X, y):
     """Calculate local statistics"""
     y_labels = list(y.columns)
 
     biased_X = sm.add_constant(X)
+    X_labels = list(biased_X.columns)
 
     local_params = []
     local_sse = []
     local_pvalues = []
     local_tvalues = []
     local_rsquared = []
+    meanY_vector, lenY_vector = [], []
 
     for column in y.columns:
-        curr_y = list(y[column])
+        curr_y = y[column]
+
+        X_, y_ = ignore_nans(biased_X, curr_y)
+        meanY_vector.append(np.mean(y_))
+        lenY_vector.append(len(y_))
 
         # Printing local stats as well
-        model = sm.OLS(curr_y, biased_X.astype(float)).fit()
+        model = sm.OLS(y_, X_).fit()
         local_params.append(model.params)
         local_sse.append(model.ssr)
         local_pvalues.append(model.pvalues)
         local_tvalues.append(model.tvalues)
-        local_rsquared.append(model.rsquared_adj)
+        local_rsquared.append(model.rsquared)
 
-    keys = ["beta", "sse", "pval", "tval", "rsquared"]
+    keys = [
+        "Coefficient", "Sum Square of Errors", "t Stat", "P-value",
+        "R Squared", "covariate_labels"
+    ]
     local_stats_list = []
 
     for index, _ in enumerate(y_labels):
         values = [
             local_params[index].tolist(), local_sse[index],
-            local_pvalues[index].tolist(), local_tvalues[index].tolist(),
-            local_rsquared[index]
+            local_tvalues[index].tolist(), local_pvalues[index].tolist(),
+            local_rsquared[index], X_labels
         ]
         local_stats_dict = {key: value for key, value in zip(keys, values)}
         local_stats_list.append(local_stats_dict)
 
         beta_vector = [l.tolist() for l in local_params]
 
-    return beta_vector, local_stats_list
+    return beta_vector, local_stats_list, meanY_vector, lenY_vector
 
 
 def add_site_covariates(args, X):
@@ -135,7 +171,8 @@ def add_site_covariates(args, X):
     site_df = pd.DataFrame(site_matrix, columns=site_covar_list)
 
     select_cols = [
-        col for col in site_df.columns if args["state"]["clientId"] in col
+        col for col in site_df.columns
+        if args["state"]["clientId"] in col[len('site_'):]
     ]
 
     site_df[select_cols] = 1
